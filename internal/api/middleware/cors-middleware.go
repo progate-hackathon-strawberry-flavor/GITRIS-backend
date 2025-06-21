9a2580984e61b4c2b87f4ba7417063d367b089c5
Riochin <118029.ichikama@gmail.com>
2025-06-21 13:00:16 +0900

    add: デプロイ用のcors設定を追加

--- internal/api/middleware/cors-middleware.go
+++ internal/api/middleware/cors-middleware.go
@@ -9,7 +9,7 @@ import (
 // CORSHandler はCORS設定を適用するミドルウェアを返します。
 func CORSHandler() func(http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"}, // フロントエンドのオリジン
+		AllowedOrigins:   []string{"http://localhost:3000","https://gitris-frontend-deploy.vercel.app"}, // フロントエンドのオリジン
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
