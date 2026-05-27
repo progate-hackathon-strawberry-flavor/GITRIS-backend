a4f825db8b7102ffa60bf0477cf7742c4b6b76f6
wenseulrenjy <middollisum@gmail.com>
2026-05-27 15:07:42 +0900

    CORS設定にAWS ELBのオリジンを追加

--- internal/api/middleware/cors-middleware.go
+++ internal/api/middleware/cors-middleware.go
@@ -9,7 +9,7 @@ import (
 // CORSHandler はCORS設定を適用するミドルウェアを返します。
 func CORSHandler() func(http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "https://gitris-frontend-mauve.vercel.app", "https://gitris-frontend-deploy.vercel.app"}, // フロントエンドのオリジン
+		AllowedOrigins:   []string{"http://localhost:3000", "https://gitris-frontend-mauve.vercel.app", "https://gitris-frontend-deploy.vercel.app", "http://a56b195d9968f44b4a463cbfb45d0232-757013918.ap-northeast-1.elb.amazonaws.com"}, // フロントエンドのオリジン
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
